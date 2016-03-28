4375fc1e74daa524c9b5eebde2f26a64818ed36f
huiyugeng <interhuiyg@163.com>
2016-03-28 17:06:24 +0800

    Add top to linux and Add check_cmd to decorator

--- py_sys/utils/decorator.py
+++ py_sys/utils/decorator.py
@@ -13,6 +13,16 @@ def check_os(os_list):
         return wrapper
     return decorators
 
+def check_cmd(cmd_list):
+    def decorators(func):
+        def wrapper(*args, **kwargs):
+            for cmd in cmd_list:
+                if os.system(cmd) != 0:
+                    raise IOError('Command check fail : ' + cmd)
+            return func( *args , **kwargs)
+        return wrapper
+    return decorators
+
 def check_file(filename):
     def decorators(func):
         def wrapper(*args, **kwargs):
