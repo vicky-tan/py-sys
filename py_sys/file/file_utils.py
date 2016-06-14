de5d132fdf1fb9ddbc9625c0666d799550ee25c8
Yugeng Hui <interhuiyg@163.com>
2016-06-15 00:59:57 +0800

    Add file_utils stub

---
+++ py_sys/file/file_utils.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+
+import os
+
+class FileUtils(object):
+
+    def __init__(self):
+        pass
+    
+    def test_file(self, filename, chmod):
+        pass
+    
+    def get_dir_size(self, dir_path):
+        pass
+    
+    def cp(self, src, dst):
+        pass
+    
+    def mv(self, src, dst):
+        pass
+    
+    def cat(self, filename):
+        pass
+    
+    
+        
